3d275cbfbc6cf8f1b950bf8fe9779964a5efad5d
João Campos <joaog.taborda.campos@gmail.com>
2022-03-09 02:20:48 -0300

    Simplificando codigo usando features do typescript

--- typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
@@ -3,11 +3,14 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
   private negociacoes: Array<Negociacao> = [];
 
+// You can change Array<Negociacao> for Negociacao[], they do the same thing on the code. 
+
   adiciona(negociacao: Negociacao){
     this.negociacoes.push(negociacao);
 
   }
 
+  // You can change ReadonlyArray<Negociacao> for  "readonly Negociacao[]", they do the same thing on the code, is just another sintax display on typescript
   lista(): ReadonlyArray<Negociacao> {
     return this.negociacoes
   }
