3810f8bcb04680888602029773447ce0f2ec689d
João Campos <joaog.taborda.campos@gmail.com>
2022-05-19 21:09:41 -0300

    Adionando validação de dia util ao adicionar negociação

--- typescript-curso-1-arquivos-iniciais/dist/js/controllers/negociacao-controller.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/controllers/negociacao-controller.js
@@ -15,9 +15,14 @@ export class NegociacaoController {
     adiciona() {
         const negociacao = this.criaNegociacao();
         console.log(negociacao);
-        this.negociacoes.adiciona(negociacao);
-        this.limparFormulario();
-        this.atualizaView();
+        if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6) {
+            this.negociacoes.adiciona(negociacao);
+            this.limparFormulario();
+            this.atualizaView();
+        }
+        else {
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas!');
+        }
     }
     criaNegociacao() {
         const exp = /-/g;
